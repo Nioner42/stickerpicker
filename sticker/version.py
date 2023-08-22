eed24e94879aa1aa79f20de3fa30324785cfe996
Nioner42 <nioner@proton.me>
2023-08-22 19:23:03 +0200

    deo test

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '345e227f'
+version = '0.1.0+dev.345e227f'
+linkified_version = '0.1.0+dev.[345e227f](https://github.com/maunium/stickerpicker/commit/345e227fbf383c38c872e62ec476f8a93c0bf4c5)'
